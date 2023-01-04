84cce1da52ea34f2e8a56ac4ccbf846a03981d5b
diegostafa <dstafa.dev@gmail.com>
2023-01-04 23:21:22 +0100

    join game: ui draft

--- lib/ui/screen/join_game_screen.dart
+++ lib/ui/screen/join_game_screen.dart
@@ -1,10 +1,17 @@
+import 'package:batonchess/ui/screen/game_screen.dart';
 import 'package:flutter/material.dart';
 
-// select a game
-// retrieve the gameProps for the selected game
-// show dialog "join as <SIDE>"
-// post /join gameId, playerId, side -> return gameState
-// gameScreen(gameState)
+/*
+  - select a game
+  - retrieve the gameProps for the selected game
+  - show dialog "join as <SIDE>"
+  - post /join gameId, playerId, side -> return gameState
+  - gameScreen(gameState)
+
+  ui:
+  - scaffold
+    - local tab
+*/
 
 class JoinGameScreen extends StatelessWidget {
   const JoinGameScreen({super.key});
@@ -12,9 +19,32 @@ class JoinGameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Join a game"),
-        ),
+      appBar: AppBar(
+        title: const Text("Join a game"),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const GameScreen()),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text('title'),
+                  subtitle: Text('subtitle'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
